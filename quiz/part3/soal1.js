4f884a17611d1757e817d677ea2c98c3f1af634f
rafizein <rafizein90@gmail.com>
2025-02-28 16:58:41 +0700

    part3

---
+++ quiz/part3/soal1.js
@@ -0,0 +1,20 @@
+let i=1;
+
+console.log("LOOPING PERTAMA")
+
+while (i<=5) {
+    console.log (i);
+    i++
+    
+}
+
+
+let j=5
+
+console.log("LOOPING KEDUA")
+
+while (j>=1) {
+    console.log(j)
+    j--
+    
+}
---
+++ quiz/part3/soal4.js
@@ -0,0 +1,32 @@
+{
+    console.log("KELIPATAN 3 PENAMBAHAN 2")
+}
+for (let i = 1; i <= 100; i +=2)
+    if (i % 3 === 0) { 
+        console.log(i +": KELIPATAN 3");}
+    else {
+        console.log(i)
+    }
+
+{
+    console.log("KELIPATAN 6 PENAMBAHAN 5")
+}
+for (let j = 1; j <= 100; j +=5)
+    if (j % 6 === 0) { 
+        console.log(j + ": KELIPATAN 6");
+}
+else {
+    console.log(j)
+}
+
+{
+    console.log("KELIPATAN 10 PENAMBAHAN 9")
+}
+for (let k = 1; k <= 100; k +=9)
+    if (k % 10 === 0) { 
+        console.log(k + ": KELIPATAN 10");
+}
+else{
+    console.log(k)
+}
+    
